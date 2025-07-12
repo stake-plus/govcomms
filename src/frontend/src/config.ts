b4505568bdcde4a8232fea25aa83830a5b7b4702
Tom <tsenseless@gmail.com>
2025-07-12 19:37:02 -0400

    Update

--- src/frontend/src/config.ts
+++ src/frontend/src/config.ts
@@ -10,8 +10,8 @@ interface Config {
 const isDevelopment = import.meta.env.MODE === 'development';
 
 const config: Config = {
-  apiUrl: import.meta.env.VITE_API_URL || (isDevelopment ? 'https://ocp.reeeeeeeeee.io:9001/v1' : window.location.origin + '/v1'),
-  gcUrl: import.meta.env.VITE_GC_URL || (isDevelopment ? 'https://ocp.reeeeeeeeee.io:9001/v1' : window.location.origin),
+  apiUrl: import.meta.env.VITE_API_URL || 'https://ocp.reeeeeeeeee.io:9001/v1',
+  gcUrl: import.meta.env.VITE_GC_URL || 'https://ocp.reeeeeeeeee.io:9001',
   walletConnectProjectId: import.meta.env.VITE_WC_PROJECT_ID || '60579b65953a7b91dbe19366e383d8bb',
   walletConnectName: 'REEEEEEEEEE DAO - Opengov Communications Platform',
   walletConnectDescription: 'Connect with REEEEEEEEEE DAO to discuss your polkadot oepngov proposal.'
